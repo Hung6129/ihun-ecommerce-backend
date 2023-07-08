6166d79c85b9c68a890f4641240a51e2dacebbaa
HungNguyenPQ <quyhung6129@gmail.com>
2023-07-09 00:38:13 +0700

    step 2

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -80,7 +81,7 @@ DATABASES = {
         "USER": "fullstackecommerce",
         "PASSWORD": "fullstackecommerce",
         "HOST": "localhost",
-        "PORT": "5432"
+        "PORT": "5432",
     }
 }
 
